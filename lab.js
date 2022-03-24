6912e77c6db66e15ceb44a698e4cf7b14218a1bb
BrandotheCluff <brandon.cluff2@gmail.com>
2022-03-24 09:31:59 -0600

    Making-Bad-Decisions

--- lab.js
+++ lab.js
@@ -1,26 +1,89 @@
+//Problem 1
+
 // let lovesCode = true
 // if(lovesCode = true ){
 //   console.log("I love to code!")
 // }else if(lovesCode = false)
 //   console.log("Debatable")
 
-let amysAge = 29
-let brittanysAge = 34
-let amysBirthYear = 1991
-let brittanysBirthYear = 1986
+//Problem 2 and 3
 
-if(brittanysAge > amysAge){
-  console.log("Brittany is older than Amy.")
-}else if(amysAge > brittanysAge){
-  console.log("Amy is older than Brittany.")
-}else{
-  console.log("They are the same age.")
+// let amysAge = 29
+// let brittanysAge = 34
+// let amysBirthYear = 1991
+// let brittanysBirthYear = 1986
+
+// if(brittanysAge > amysAge){
+//   console.log("Brittany is older than Amy.")
+// }else if(amysAge > brittanysAge){
+//   console.log("Amy is older than Brittany.")
+// }else{
+//   console.log("They are the same age.")
+// }
+
+// if(amysBirthYear === brittanysBirthYear){
+//   console.log("Amy and Brittany were born in the same year")
+// }else{
+//   console.log("They were not born in the same year")
+// }
+
+//Problem 4
+
+// let tempurature = 92
+// let rain = false 
+
+// if(tempurature >= 80 && rain === true){
+//   console.log("Wear a t-shirt and bring an Umberella")
+// }else if(tempurature > 60 && tempurature < 80 && rain === true){
+//   console.log("Wear a rain jacket")
+// }else if(tempurature <= 60){
+//   console.log("Bring an umberella and wear a jacket")
+// }else if(tempurature >= 80 && rain === false){
+//   console.log("Wear a t-shirt")
+// }else if(tempurature > 60 && tempurature < 80 && rain === false){
+//   console.log("Wear a jacket")
+// }
+
+//Problem 5
+
+//for (let i = 0; i < 10; i++){
+  //console.log(hello)
+//}
+
+//Problem 6 
+
+// for (let i = 1; i < 11; i++){
+//   console.log([i])
+// }
+
+//Problem 7
+
+// for (let i = 10; i > -1; i--){
+//   console.log([i])
+// }
+
+//Problem 8
+
+// let score = 0;
+// let passingScore = 7;
+// while (score < 7){
+//   console.log("Your score is not high enough")
+//   score += 1
+// }
+
+//Problem 9 and 10
+
+// let changeMyMind = false
+// if(changeMyMind){
+//   changeMyMind = !changeMyMind
+// }else{
+//     changeMyMind = !changeMyMind
+//     } console.log(changeMyMind)
+  
+//Problem 11
+
+let z = 5
+while (z > 0){
+  console.log(z)
+  z--
 }
-
-if(amysBirthYear === brittanysBirthYear){
-  console.log("Amy and Brittany were born in the same year")
-}else{
-  console.log("They were not born in the same year")
-}
-
-
